ecfc38db1f02036116e8705a019cee6ddf8737ea
Callum Byrne <callumjbyrne97@gmail.com>
2022-04-26 15:28:40 +1000

    menu fixed back to inset

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,6 +1,6 @@
 const Menu = () => {
   return (
-    <nav className="fixed top-0 z-40 flex w-full flex-col items-center justify-center overflow-hidden bg-black py-24 text-[20vw] font-bold text-white">
+    <nav className="fixed inset-0 z-40 flex w-full flex-col items-center justify-center overflow-hidden bg-black py-24 text-[20vw] font-bold text-white">
       <div>Home</div>
       <div>About</div>
       <div>Projects</div>
